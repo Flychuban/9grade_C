c1e8bed1085e163990d491823f8d07907cf440ac
Kaloyan Anastasov <kaloanas07@gmail.com>
2023-06-06 21:04:49 +0300

    added new samples

--- data-serialization-exercise/write_data.c
+++ data-serialization-exercise/write_data.c
@@ -10,21 +10,25 @@ struct Employee
 
 
 int main(){
-    struct Employee director = {"Ivan", 2, NULL};
+    struct Employee director = {"Ivan", 3, NULL};
     struct Employee manager_1 = {"Pesho", 3, NULL};
     struct Employee manager_2 = {"Gosho", 3, NULL};
-
+    struct Employee manager_3 = {"Tosho", 4, NULL};
     struct Employee employee_1 = {"Marin", 0, NULL};
     struct Employee employee_2 = {"Kaloyan", 0, NULL};
     struct Employee employee_3 = {"Hristo", 0, NULL};
     struct Employee employee_4 = {"Pavel", 0, NULL};
     struct Employee employee_5 = {"Dimo", 0, NULL};
     struct Employee employee_6 = {"Stefan", 0, NULL};
-
+    struct Employee employee_7 = {"Miro", 0, NULL};
+    struct Employee employee_8 = {"Ivo", 0, NULL};
+    struct Employee employee_9 = {"Kristian", 0, NULL};
+    struct Employee employee_10 = {"Vasil", 0, NULL};
 
     director.employees = (struct Employee **)malloc(sizeof(struct Employee *) * director.number_employees);
     director.employees[0] = &manager_1;
     director.employees[1] = &manager_2;
+    director.employees[2] = &manager_3;
 
     manager_1.employees = (struct Employee **)malloc(sizeof(struct Employee *) * manager_1.number_employees);
     manager_1.employees[0] = &employee_1;
@@ -35,6 +39,20 @@ int main(){
     manager_2.employees[0] = &employee_4;
     manager_2.employees[1] = &employee_5;
     manager_2.employees[2] = &employee_6;
+
+    manager_3.employees = (struct Employee **)malloc(sizeof(struct Employee *) * manager_3.number_employees);
+    manager_3.employees[0] = &employee_7;
+    manager_3.employees[1] = &employee_8;
+    manager_3.employees[2] = &employee_9;
+    manager_3.employees[3] = &employee_10;
+
+    // for (int i = 0; i < director.number_employees; i++)
+    // {
+    //     director.employees = (struct Employee **)malloc(sizeof(struct Employee *) * director.number_employees);
+    //     director.employees[i] = &manager_1;  // problem with dynamic changing variables in order now its hardcoded
+
+    // }
+    
 
     FILE *fp = fopen("employees.bin", "wb");
     if (fp == NULL)
