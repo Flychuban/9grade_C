288fb898b65a6161ae3e0f7ca5e7edc7293e9ee6
Kaloyan Anastasov <kaloanas07@gmail.com>
2023-06-06 20:57:48 +0300

    Successful read

--- data-serialization-exercise/write_data.c
+++ data-serialization-exercise/write_data.c
@@ -13,12 +13,14 @@ int main(){
     struct Employee director = {"Ivan", 2, NULL};
     struct Employee manager_1 = {"Pesho", 3, NULL};
     struct Employee manager_2 = {"Gosho", 3, NULL};
+
     struct Employee employee_1 = {"Marin", 0, NULL};
     struct Employee employee_2 = {"Kaloyan", 0, NULL};
     struct Employee employee_3 = {"Hristo", 0, NULL};
     struct Employee employee_4 = {"Pavel", 0, NULL};
     struct Employee employee_5 = {"Dimo", 0, NULL};
     struct Employee employee_6 = {"Stefan", 0, NULL};
+
 
     director.employees = (struct Employee **)malloc(sizeof(struct Employee *) * director.number_employees);
     director.employees[0] = &manager_1;
@@ -41,17 +43,20 @@ int main(){
         exit(1);
     }
     
+    fwrite(director.name, sizeof(char), 100, fp);
+    fwrite(&director.number_employees, sizeof(int), 1, fp);
+    
     for (int i = 0; i < director.number_employees; i++)
     {
         char *cur_man_name = director.employees[i]->name;
         int cur_man_number_employees = director.employees[i]->number_employees;
-        fwrite(&cur_man_name, sizeof(char), 100, fp);
+        fwrite(cur_man_name, sizeof(char), 100, fp);
         fwrite(&cur_man_number_employees, sizeof(int), 1, fp);
         for (int j = 0; j < cur_man_number_employees; j++)
         {
             char *cur_emp_name = director.employees[i]->employees[j]->name;
             int cur_emp_number_employees = director.employees[i]->employees[j]->number_employees;
-            fwrite(&cur_emp_name, sizeof(char), 100, fp);
+            fwrite(cur_emp_name, sizeof(char), 100, fp);
             fwrite(&cur_emp_number_employees, sizeof(int), 1, fp);
         }   
     }
